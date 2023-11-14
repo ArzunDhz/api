a5f05803c3cd91918598b44459e785b63e938807
ArzunDhz <86089364+ArzunDhz@users.noreply.github.com>
2023-11-14 11:08:39 +0545

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT;
 
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000","https://fauro-next.vercel.app","*"],
     methods: "*",
   })
 );
